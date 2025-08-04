3fbbcdc5ae77c9d0772bffcdef70be66dba9ad40
Infant Ashil A <infantashil55@gmail.com>
2025-08-04 14:58:18 +0530

    code

---
+++ reactapp/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={styles.container}>
+      <h1>Welcome to My CRM Dashboard</h1>
+      <p>This is a sample React component.</p>
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    padding: '2rem',
+    fontFamily: 'Arial',
+    textAlign: 'center',
+  },
+};
+
+export default App;
