9638ae42da894488ab7f06d9153bde2441e36600
Infant Ashil A <infantashil55@gmail.com>
2025-08-05 10:29:02 +0530

    code

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrmSystemApplication {
+public class SpringappApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CrmSystemApplication.class, args);
+        SpringApplication.run(SpringappApplication.class, args);
     }
 }
